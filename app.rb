0e8a90894f3e868a59a3401689e4f610f4b820c3
Laura Felce <crepu83@yahoo.it>
2019-03-15 10:36:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,5 +21,11 @@ class App < Sinatra::Base
   "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
+  get '/:operation/:number1/:number2' do
+    @add = params[:number1] + params[:number2]
+    @subtract = params[:number1] - params[:number2]
+    @multiply = params[:number1] * params[:number2]
+    @divide = 
+  end
 
 end
