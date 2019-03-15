6471581b34e673953174e11b8b3a12ce97c92d08
Laura Felce <crepu83@yahoo.it>
2019-03-15 10:26:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,6 +17,6 @@ class App < Sinatra::Base
     "#{@phrase}\n" * @num
   end
 
-
-
+  get "/say/:word1/:word2/:word3/:word4/:word5"
+  params[:word1]20%params[:word2]20%params[:word3]20%params[:word4]20%params[:word5]
 end
